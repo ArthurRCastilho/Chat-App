9c4526646b2f1182d4bafa37be72801f8e2e21d6
ArthurLVidal <arthurodri2017@gmail.com>
2024-07-07 16:56:23 -0300

    Otimização

--- lib/core/services/auth/auth_service.dart
+++ lib/core/services/auth/auth_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/auth/auth_mock_service.dart';
 
 abstract class AuthService {
   ChatUser? get currentUser;
@@ -18,6 +19,10 @@ abstract class AuthService {
     String email,
     String password,
   );
-  
+
   Future<void> logout();
+
+  factory AuthService() {
+    return AuthMockService();
+  }
 }
